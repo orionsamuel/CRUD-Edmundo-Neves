b039a9dc188f8dd03a34d66a0a154665e8f4acc7
orionsamuel <s.oliveira.15@hotmail.com>
2021-06-01 16:26:14 -0300

    Parte de read e create Ok

--- app/models.py
+++ app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Alunos(models.Model):
     pasta = models.IntegerField()
-    ano = models.DateField()
+    ano = models.IntegerField()
     nome = models.CharField(max_length=150)
     filiacao = models.CharField(max_length=100)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,22 @@
 from app.models import Alunos
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from app.forms import AlunosForm
+from app.models import Alunos
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db'] = Alunos.objects.all()
+    return render(request, 'index.html', data)
 
 def form(request):
     data = {}
     data['form'] = AlunosForm()
     return render(request, 'form.html', data)
+
+def create(request):
+    form = AlunosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
