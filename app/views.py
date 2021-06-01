22a51357e2e05e4bef45fcbd4c85416396ce2d12
orionsamuel <s.oliveira.15@hotmail.com>
2021-06-01 19:17:51 -0300

    Parte do update funcionando

--- app/views.py
+++ app/views.py
@@ -20,3 +20,17 @@ def create(request):
     if form.is_valid():
         form.save()
         return redirect('home')
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Alunos.objects.get(pk=pk)
+    data['form'] = AlunosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data = {}
+    data['db'] = Alunos.objects.get(pk=pk)
+    form = AlunosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form, create
+from app.views import home, form, create, edit, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('form/', form, name='form'),
     path('create/', create, name='create'),
+    path('edit/<int:pk>', edit, name='edit'),
+    path('update/<int:pk>', update, name='update'),
 ]
