438d6cc209f1153536b90212d4fdc88ee7e60ace
orionsamuel <s.oliveira.15@hotmail.com>
2021-06-04 09:05:41 -0300

    Adição do arquivo runtime.txt

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
